d8c94b2a55d93022ce728297f88007a2480d82b3
anasce <97723156+anasce@users.noreply.github.com>
2024-04-23 07:59:46 +0200

    Update asimon.js

--- asimon.js
+++ asimon.js
@@ -28,16 +28,6 @@ function animatePress(cc) {
     }, 100);
 };
 
-function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length !== b.length) return false;
-    for (var i = 0; i < a.length; ++i) {
-        if (a[i] !== b[i]) return false;
-    }
-    return true;
-}
-
 function igra() {
     userClickedPattern = [];
     dugmad.forEach((pojDugme) => {
